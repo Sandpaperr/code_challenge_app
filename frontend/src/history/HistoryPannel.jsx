a66a981a5c37b94bd4415fc4d2c7c65034f7fc1b
Leandro Russo <russo.leand@gmail.com>
2025-07-07 14:39:02 +0100

    Finished historyPannel coponent

--- frontend/src/history/HistoryPannel.jsx
+++ frontend/src/history/HistoryPannel.jsx
@@ -1,5 +1,46 @@
 import "react"
+import { useState, useEffect } from "react"
+import { MCQChallenge } from "../challenge/MCQChallenge"
 
 export function HistoryPannel(){
-    return <></>
+    const [history, setHistory] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError ] = useState(null)
+
+    // As soon as the component load, fetch History
+    useEffect(() => {
+        fetchHistory()
+
+    }, [])
+
+    const fetchHistory = async() => {
+        setIsLoading(false)
+    }
+
+    if (isLoading){
+        return <div className="loading">Loading History...</div>
+    }
+
+    if (error){
+        return <div>
+            <p>{error}</p>
+            <button onClick={fetchHistory}>Retry</button>
+        </div>
+    }
+
+    return <div className="history-panel">
+        <h2>History</h2>
+        {history.length === 0 ? <p>No challenge history</p> : 
+            <div className="history-list">
+                {history.map((challenge) =>{
+                    return <MCQChallenge challenge={challenge} 
+                            key={challenge.id} 
+                            showExplaination
+                        />
+                    })
+                }
+            </div>
+        
+        }
+    </div>
 }
